91966075df414de8a299f36aee55c4dbc0246951
Ichisada Shioko <ichisadashioko@gmail.com>
2019-11-12 17:36:24 +0700

    :sparkles: check location permission

--- app/src/main/java/com/example/gpstracking/MainActivity.java
+++ app/src/main/java/com/example/gpstracking/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.gpstracking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity {
+    private FusedLocationProviderClient fusedClient;
+    private TextView txtLocation;
+    private static final int FUSED_LOCATION_CLIENT_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.txtLocation = (TextView) findViewById(R.id.txtLocation);
+
+        fusedClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // check permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+            }, FUSED_LOCATION_CLIENT_PERMISSION_CODE);
+        }
     }
 }
