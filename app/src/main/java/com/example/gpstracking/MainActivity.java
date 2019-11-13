0e626be4294367da20268eb41e9b404056cdec3c
Ichisada Shioko <ichisadashioko@gmail.com>
2019-11-13 08:28:40 +0700

    :boom: FusedLocationProviderClient only available with Google Play Services

--- app/src/main/java/com/example/gpstracking/MainActivity.java
+++ app/src/main/java/com/example/gpstracking/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gpstracking;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -7,14 +8,21 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int FUSED_LOCATION_CLIENT_PERMISSION_CODE = 1;
+    private static final String[] REQUIRED_PERMISSION = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+    };
+
     private FusedLocationProviderClient fusedClient;
     private TextView txtLocation;
-    private static final int FUSED_LOCATION_CLIENT_PERMISSION_CODE = 1;
+    private double wayLatitude, wayLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +36,35 @@ public class MainActivity extends AppCompatActivity {
         // check permission
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-            }, FUSED_LOCATION_CLIENT_PERMISSION_CODE);
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSION, FUSED_LOCATION_CLIENT_PERMISSION_CODE);
+        } else {
+            fusedClient.getLastLocation().addOnSuccessListener(this, location -> {
+                if (location != null) {
+                    wayLatitude = location.getLatitude();
+                    wayLongitude = location.getLongitude();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case FUSED_LOCATION_CLIENT_PERMISSION_CODE: {
+                // if request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fusedClient.getLastLocation().addOnSuccessListener(this, location -> {
+                        if (location != null) {
+                            wayLatitude = location.getLatitude();
+                            wayLongitude = location.getLongitude();
+                        }
+                    });
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
         }
     }
 }
